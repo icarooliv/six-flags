7a1497665dc5966cdca46359e03be8042065fdf1
Icaro Oliveira <icarooliv2@gmail.com>
2018-10-14 21:55:12 -0300

    adds roller coasters input values to array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
 
+class Brinquedo {
+  constructor(nome, duracao, pontuacao) {
+    this.nome = nome;
+    this.duracao = duracao;
+    this.pontuacao = pontuacao;
+  }
+  get div() {
+    return this.calculaDiv();
+  }
+  /*
+  o div representa o brinquedo com maior "importancia": 
+  quanto maior a pontuaçao e menor a duracao, melhor
+  */
+  calculaDiv() {
+    return this.pontuacao / this.duracao;
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -10,8 +28,10 @@ class App extends Component {
       tempo: "",
       showComponent: false
     };
+
     this.showValue = this.showValue.bind(this);
     this.showComponentOnClick = this.showComponentOnClick.bind(this);
+    this.algorithm = this.algorithm.bind(this);
   }
 
   showValue = e => {
@@ -31,17 +51,93 @@ class App extends Component {
 
     for (var i = 0; i < times; i++) {
       inputs.push(
-        <div key={i}>
+        <div id={`brinquedo${i}`} key={i}>
           <h2> Brinquedo {i + 1}</h2>
-          <input placeholder="Nome do Brinquedo" />
+          <input id={`nomeBrinquedo${i}`} placeholder="Nome do Brinquedo" />
           <p />
-          <input placeholder="Duração do Brinquedo" />
+          <input
+            id={`duracaoBrinquedo${i}`}
+            placeholder="Duração do Brinquedo"
+          />
           <p />
-          <input placeholder="Pontuação do Brinquedo" />
+          <input
+            id={`pontuacaoBrinquedo${i}`}
+            placeholder="Pontuação do Brinquedo"
+          />
         </div>
       );
     }
     return inputs;
+  }
+
+  // algorithm() {
+  //   var numeroTotalBrinquedos = 1,
+  //     tempoTotalEquipe = 1,
+  //     instancias = 1;
+
+  //     var brinquedos = new Array();
+
+  //     numeroTotalBrinquedos = this.state.quantidade;
+  //     tempoTotalEquipe = this.state.tempo;
+
+  //     if (numeroTotalBrinquedos == 0) {
+  //       alert("N nao pode ser 0!");
+  //       break;
+  //     }
+
+  //     for (var i = 0; i < numeroTotalBrinquedos; i++) {
+  //       var b = new Brinquedo();
+  //       b.duracao = rls.questionInt(`Insira a duração do brinquedo ${i}: `);
+  //       b.pontuacao = rls.questionInt(`Insira a pontuação do brinquedo ${i}: `);
+  //       brinquedos.push(b);
+  //     }
+
+  //     brinquedos.sort((a, b) => b.div > a.div);
+
+  //     console.log("Brinquedos inseridos:", brinquedos);
+
+  //     var pontuacaoTotal = 0,
+  //       tempoTotal = 0;
+  //     var k = 0;
+  //     var escolhidos = Array();
+  //     while (k < numeroTotalBrinquedos) {
+  //       var auxTempoTotal = tempoTotal + brinquedos[k].duracao;
+  //       if (auxTempoTotal <= tempoTotalEquipe) {
+  //         pontuacaoTotal += brinquedos[k].pontuacao;
+  //         tempoTotal = auxTempoTotal;
+  //         escolhidos.push(brinquedos[k]);
+  //       } else k++;
+  //     }
+
+  //     console.log("Brinquedos escolhidos: \n", escolhidos);
+  //     console.log(`Instancia:${instancias++}`);
+  //     console.log(pontuacaoTotal);
+  //     console.log("\n\n");
+  //   }
+
+  algorithm() {
+    var numeroTotalBrinquedos = 0,
+      tempoTotalEquipe = 1,
+      instancias = 1;
+
+    var brinquedos = new Array();
+
+    numeroTotalBrinquedos = this.state.quantidade;
+    tempoTotalEquipe = this.state.tempo;
+
+    if (numeroTotalBrinquedos == 0) {
+      alert("Numero de brinquedos NÃO pode ser 0!");
+      return;
+    }
+
+    for (var i = 0; i < numeroTotalBrinquedos; i++) {
+      var b = new Brinquedo();
+      b.nome = document.getElementById(`nomeBrinquedo${i}`).value;
+      b.duracao = document.getElementById(`duracaoBrinquedo${i}`).value;
+      b.pontuacao = document.getElementById(`pontuacaoBrinquedo${i}`).value;
+      brinquedos.push(b);
+    }
+    console.log(brinquedos);
   }
 
   render() {
@@ -76,7 +172,7 @@ class App extends Component {
         </div>
         <p />
         <div>
-          <button>Confirmar</button>
+          <button onClick={this.algorithm}>Confirmar</button>
         </div>
       </div>
     );
